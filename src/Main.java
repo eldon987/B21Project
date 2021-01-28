ba9e376b4e0326b46f74fda3f4f584ee07d6c469
eldon987 <eldon987@gmail.com>
2021-01-28 16:41:20 -0500

    Applied Hello feature

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // B21 are mostly good at IntelliJ
         Hello hello = new Hello(); // Prints hello
+        hello.variationOne();
     }
 }
